0f592ff67709355e4ed7aa071fce3d165e74d8b5
Zyaire <2432350452@qq.com>
2024-03-15 19:26:26 +0800

    day_3_13_认证异常

--- ZyaireBlog/zyaire-blog/src/main/java/com/zyaire/config/SecurityConfig.java
+++ ZyaireBlog/zyaire-blog/src/main/java/com/zyaire/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.zyaire.config;
 
+import com.zyaire.filter.JwtAuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,9 +10,19 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+    @Autowired
+    AuthenticationEntryPoint authenticationEntryPoint;
+    @Autowired
+    AccessDeniedHandler accessDeniedHandler;
 
     @Override
     @Bean
@@ -34,9 +46,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
 // 对于登录接口 允许匿名访问
                 .antMatchers("/login").anonymous()
+                .antMatchers("/article/hotArticleList").authenticated()
 // 除上面外的所有请求全部不需要认证即可访问
                 .anyRequest().permitAll();
+
+        http.exceptionHandling()
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler);
         http.logout().disable();
+
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
 //允许跨域
         http.cors();
     }
---
+++ ZyaireBlog/zyaire-framework/src/main/java/com/zyaire/handler/security/AuthenticationEntryPointImpl.java
@@ -0,0 +1,33 @@
+package com.zyaire.handler.security;
+
+import com.alibaba.fastjson.JSON;
+import com.zyaire.domain.ResponseResult;
+import com.zyaire.enums.AppHttpCodeEnum;
+import com.zyaire.utils.WebUtils;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class AuthenticationEntryPointImpl implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        e.printStackTrace();
+        ResponseResult responseResult = null;
+        if (e instanceof BadCredentialsException){
+            responseResult = ResponseResult.errorResult(AppHttpCodeEnum.LOGIN_ERROR.getCode(), e.getMessage());
+        } else if (e instanceof InsufficientAuthenticationException) {
+            responseResult = ResponseResult.errorResult(AppHttpCodeEnum.NEED_LOGIN);
+        } else {
+            responseResult = ResponseResult.errorResult(AppHttpCodeEnum.SYSTEM_ERROR.getCode(), "认证或授权失败");
+        }
+        WebUtils.renderString(httpServletResponse, JSON.toJSONString(responseResult));
+    }
+}
